2f6d277b1a1a93d0073e92845cb231685696c51b
Miles Buchwaldt <buchwaldtm@gmail.com>
2019-07-10 16:03:07 -0600

    Added to README and setup script.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # UNIXBasics2019
 Workshop for the AAFC Bioinformatics Meeting in Lethbridge
+
+## How to use:
+From a BioCluster terminal window, clone this repository:
+```
+git clone https://github.com/mrbuchwaldt/UNIXBasics2019.git
+bash UNIXBasics2019/setup.sh
+```
+
+This should create a directory structure that looks like:
+```
+
+```
--- setup.sh
+++ setup.sh
@@ -1,3 +1,34 @@
 #!/usr/bin/bash
 
+#We want a few folders and files for the users to play with.
+#Also download some data to use for a small genomics demonstration.
+
+# Here is what the home directory should look like:
+# /path/to/testuser/
+#    |-bin/
+#    |-
+#    |-ReferenceGenome/
+#    |    |-Organism.fasta
+#    |    |-Organism.annotation.gff3
+#    |-RNASeq/
+#    |    |-Sample1_R1.fq.gz
+#    |    |-Sample1_R2.fq.gz
+#    |    |-Sample2_R1.fq.gz
+#    |    |-Sample2_R2.fq.gz
+#    |    |-Sample3_R1.fq.gz
+#    |    |-Sample3_R2.fq.gz
+#    |-scripts/
+#    |-setup.sh
+
+
+#Create directories:
+mkdir bin
+mkdir ReferenceGenome
+mkdir RNASeq
+mkdir scripts
+
+#Download Data:
+
+wget
+
 
